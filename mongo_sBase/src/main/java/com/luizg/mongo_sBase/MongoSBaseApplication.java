9b29a8db3ec4dacc86729e912881fa368ee27f99
Dev-Lu <159707892+LuizGustavo134@users.noreply.github.com>
2026-01-02 12:54:37 -0300

    Add files via upload

---
+++ mongo_sBase/src/main/java/com/luizg/mongo_sBase/MongoSBaseApplication.java
@@ -0,0 +1,13 @@
+package com.luizg.mongo_sBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSBaseApplication.class, args);
+	}
+
+}
---
+++ mongo_sBase/src/test/java/com/luizg/mongo_sBase/MongoSBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizg.mongo_sBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
